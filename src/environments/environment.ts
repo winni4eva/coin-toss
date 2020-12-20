b952a002483cb4e1042877bd8b6ee37595269ad8
winni4eva <adams4lyf@gmail.com>
2020-12-20 19:55:24 +0000

    en(assets): enabled asset images

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   coinRestHost: 'https://rest-sandbox.coinapi.io/v1',
   coinWssHost: 'wss://ws-sandbox.coinapi.io/v1',
-  coinKey: 'F79CFF39-F765-4508-8D30-D998EFFAE554',
+  coinKey: '',
 };
-
+//
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
